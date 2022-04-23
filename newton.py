ea57773f1c772faa0470787f5304e905fe450236
Echowind <Suhyeric@foxmail.com>
2022-04-23 22:19:44 +0800

    add quasi-newton method

--- newton.py
+++ newton.py
@@ -74,8 +74,50 @@ def newton(x, function, method='pure', epsilon=1e-5, verbose=10):
     return np.array(record)
 
 
+def quasi_newton(x, func, grad, method='BFGS', epsilon=1e-5, verbose=10):
+    record = [x]
+    epoch = 0
+    max_iter = 100
+
+    dim = len(x)
+    H_0 = np.eye(dim)
+    g_0 = grad(x)
+
+    while epoch < max_iter:
+        d = -np.dot(H_0, g_0)
+        alpha = wolfe(func, grad, x, d)
+        x = x + alpha * d
+        g = grad(x)
+        s = alpha * d
+        y = g - g_0
+
+        # BFGS
+        H = H_0 + (1 + np.dot(y, np.dot(H_0, y))/np.dot(s, y)) * np.dot(s.reshape(-1, 1), s.reshape(1, -1))/np.dot(s, y) \
+            - (np.dot(np.dot(s.reshape(-1, 1), y.reshape(1, -1)), H_0)
+            + np.dot(np.dot(H_0, y.reshape(-1, 1)), s.reshape(1, -1))) / np.dot(s, y)
+
+        record.append(x)
+        if (epoch + 1) % verbose == 0:
+            print("---Iteration: {}---".format(epoch + 1))
+            print("x = {}, alpha = {:.3f}, f(x) = {:.5f}".format(x, alpha, func(x)))
+        if np.linalg.norm(g) < epsilon:
+            print("---Iteration: {} Ends---".format(epoch + 1))
+            print("x = {}, alpha = {:.3f}, f(x) = {:.5f}".format(x, alpha, func(x)))
+            break
+        g_0 = g
+        H_0 = H
+        epoch += 1
+    return np.array(record)
+
+
 if __name__ == "__main__":
+    # newton
     x_0 = np.array([3, 0.2])
     result = newton(x_0, 'beale', method='line', verbose=1)
     draw_contour(beale, result)
     convergence_rate(beale, result)
+
+    # quasi-newton
+    x_0 = np.array([-1.2, 1, -1.2, 1])
+    result = quasi_newton(x_0, rosenbrock, rosen_grad, method='BFGS', verbose=10)
+    convergence_rate(rosenbrock, result)
